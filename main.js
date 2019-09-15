da233ffc4a4a52b8db90d43631a56173bf47ae1b
OanaIgnat <39847267+OanaIgnat@users.noreply.github.com>
2019-09-15 13:25:25 -0400

    added bootstrap

--- main.js
+++ main.js
@@ -1,32 +1,43 @@
-let video = document.getElementById('vid');
-let start_time = 0;
-let end_time = 0;
-var poemDisplay = document.querySelector('a');
+function main() {
+    let start_time = 0;
+    let end_time = 0;
+    const $actions = document.getElementById('actions');
+    const $video = document.getElementsByTagName('video')[0];
 
-let iframe = document.getElementById("myFrame");
+    $video.addEventListener('keydown', e => {
+        const current_time = $video.currentTime.toPrecision(1);
 
-console.log(iframe);
+        switch (e.code) {
+            case "KeyS":
+                start_time = current_time;
+                break;
+            case "KeyE":
+                end_time = current_time;
+                break;
+            case "ArrowUp":
+                console.log("ArrowUp" + current_time);
+                break;
+            case "ArrowDown":
+                console.log("ArrowDown" + current_time);
+                break;
+        }
+    });
 
-video.addEventListener('keydown', e => {
-    let curtime = video.currentTime.toPrecision(1);
+    $video.addEventListener('play', () => {
+        console.log("video playing...");
+        $video.focus();
+    });
 
-    if (e.code === "KeyS")
-        start_time = curtime;
+    fetch('actions.txt')
+        .then(response => response.text())
+        .then(actions_text => actions_text.trim().split(/\r?\n/))
+        .then(actions => {
+            actions.forEach(action => {
+                const $action = document.createElement('option');
+                $action.innerText = action;
+                $actions.appendChild($action);
+            });
+        });
+}
 
-    else if (e.code === "KeyE")
-        end_time = curtime;
-
-    else if (e.code === "ArrowUp")
-        console.log("ArrowUp" + curtime);
-
-    else if (e.code === "ArrowDown")
-        console.log("ArrowDown" + curtime);
-
-    // console.log("start_time: " + start_time);
-    // console.log("end_time: "+ end_time);
-});
-
-video.addEventListener('play', () => {
-    console.log("test");
-    document.getElementById("vid").focus();
-});
+window.addEventListener('DOMContentLoaded', () => main());
